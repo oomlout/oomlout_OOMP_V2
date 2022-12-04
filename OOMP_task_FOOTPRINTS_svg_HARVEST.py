2e09f6a61e5e10c9ba95002be455ae2d1e427757
Oom Lout <aaron@oomlout.com>
2022-12-04 11:58:59 +0000

    fixed svg names

--- OOMP_task_FOOTPRINTS_svg_HARVEST.py
+++ OOMP_task_FOOTPRINTS_svg_HARVEST.py
@@ -6,11 +6,16 @@ import OOMP_kicad_BASE
 #OOMP.makePickle()
 OOMP.loadPickle()
 
-
+import OOMP_task_FOOTPRINTS_svg
 
 
 ######## TESTING
 filter = ["Connector_PinHeader_2.54mm","Resistor_SMD","Resistor_THT","Capacitor_SMD","Capacitor_THT","Capacitor_Tantalum_SMD","Package_TO_SOT_SMD","Package_TO_SOT_SMD","Package_SO"]
 filter = [""]
 OOMP_kicad_BASE.harvestAllKicad(overwrite=False,eda=True,fpFilter=filter)
+
+
+
+
+
 
